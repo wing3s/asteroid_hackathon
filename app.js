1662af923495912bcea78baa472238ba3c6f918f
wing3s <wlee@machinezone.com>
2014-10-25 00:04:24 -0700

    add asteroid api

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var conf = require('./config/config')();
 
 var app = express();
+global.client = mysql.createConnection(conf.mysqlConfig);
+client.connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use('/', routes);
-app.use('/users', users);
+require('./routes/api')(app);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api.js
@@ -0,0 +1,24 @@
+module.exports = function(app) {
+
+    app.get('/api/asteroids/:zone/:magnitude/:name', function(req,res) {
+        var zone = req.params.zone;
+        var magnitude = req.params.magnitude;
+        var name = req.params.name;
+        var zone_query = zone === '0' ? "" : " AND zones = "+zone;
+        var magnitude_query = magnitude === '0' ? "" : " AND absolute_magnitude >= " + magnitude;
+        var name_query = name === '0' ? "" : " AND name IS NOT NULL";
+        var sql = "SELECT id, object_type, radius_a, radius_b, name, absolute_magnitude " +
+                    "FROM asteroids " +
+                    "WHERE id > 0 " +
+                    zone_query + magnitude_query + name_query;
+        var query = client.query(sql, function(err, results) {
+            if (err) {
+                console.error(err);
+                res.send(err);
+                return;
+            }
+            res.send(results);
+            console.log(query.sql);
+        });
+    });
+};
